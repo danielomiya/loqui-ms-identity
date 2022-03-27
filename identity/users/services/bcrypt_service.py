7645f82e370131c984530221159c1a3dc6a1ffc2
Daniel Omiya <danielomiya@gmail.com>
2022-03-27 17:50:04 -0300

    Refactor BcryptService

--- identity/users/services/bcrypt_service.py
+++ identity/users/services/bcrypt_service.py
@@ -3,9 +3,12 @@ from identity.domain.services.password_comparer import PasswordComparer
 from identity.domain.services.password_hasher import PasswordHasher
 
 
+ENCODING = "utf8"
+
+
 class BcryptService(PasswordComparer, PasswordHasher):
     def hash(self, passwd: str) -> str:
-        return bcrypt.hashpw(passwd.encode("utf8"), bcrypt.gensalt()).decode("utf8")
+        return bcrypt.hashpw(passwd.encode(ENCODING), bcrypt.gensalt()).decode(ENCODING)
 
     def compare(self, passwd: str, hashed: str) -> bool:
-        return bcrypt.checkpw(passwd, hashed)
+        return bcrypt.checkpw(passwd.encode(ENCODING), hashed.encode(ENCODING))
