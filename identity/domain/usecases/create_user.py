49542e6c7c09e9212c8262f6964ce63887c1d3c5
Daniel Omiya <danielomiya@gmail.com>
2022-03-28 21:44:35 -0300

    Refactor user creation use case

--- identity/domain/usecases/create_user.py
+++ identity/domain/usecases/create_user.py
@@ -1,5 +1,5 @@
+from dataclasses import asdict, dataclass
 from datetime import datetime
-from typing import TypedDict
 
 from identity.domain.errors import ValidationError
 from identity.domain.models.user import User
@@ -8,22 +8,24 @@ from identity.domain.services.password_hasher import PasswordHasher
 from identity.domain.usecases.base_use_case import UseCase
 from identity.domain.validation.cnpj_rule import CNPJRule
 from identity.domain.validation.confirmation_rule import ConfirmationRule
-from identity.domain.validation.contains_rule import ContainsRule
 from identity.domain.validation.cpf_rule import CPFRule
 from identity.domain.validation.either_rule import EitherRule
 from identity.domain.validation.minimum_age_rule import MinimumAgeRule
 from identity.domain.validation.not_empty_rule import NotEmptyRule
+from identity.domain.validation.required_rule import RequiredRule
+from identity.domain.validation.validation_handler import ValidationHandler
 
 
-class CreateUserDTO(TypedDict):
+@dataclass
+class CreateUserDTO:
     name: str
     document: str
     birth_date: datetime
     email: str
-    phone: str
     password: str
     confirm_password: str
-    picture_url: str
+    phone: str = None
+    picture_url: str = None
 
 
 class CreateUserUseCase(UseCase[CreateUserDTO, User]):
@@ -36,34 +38,28 @@ class CreateUserUseCase(UseCase[CreateUserDTO, User]):
         self.password_hasher = password_hasher
 
     def execute(self, request: CreateUserDTO) -> User:
-        validator = ContainsRule(
-            "name",
-            "document",
-            "birth_date",
-            "email",
-            "password",
-            "confirm_password",
-        )
-        (
-            validator.next(NotEmptyRule("name"))
-            .next(NotEmptyRule("document"))
-            .next(NotEmptyRule("birth_date"))
-            .next(NotEmptyRule("email"))
-            .next(ConfirmationRule("password", "confirm_password"))
-            .next(EitherRule(CPFRule("document"), CNPJRule("document")))
-            .next(MinimumAgeRule("birth_date", 18))
+        validator = (
+            ValidationHandler[CreateUserDTO]()
+            .add("name", [RequiredRule, NotEmptyRule])
+            .add("document", [RequiredRule, NotEmptyRule, EitherRule(CPFRule, CNPJRule)])
+            .add("birth_date", [RequiredRule, MinimumAgeRule(18)])
+            .add("email", [RequiredRule, NotEmptyRule])
+            .add("password", [RequiredRule])
+            .add("confirm_password", [RequiredRule, ConfirmationRule("password")])
         )
 
-        err = validator.handle(request)
-        if err:
-            raise ValidationError(error=err)
+        errors = validator.validate(request)
+        if errors:
+            raise ValidationError(error=errors)
 
-        hashed_password = self.password_hasher.hash(request["password"])
+        hashed_password = self.password_hasher.hash(request.password)
 
+        entity = asdict(request)
         for key in ("password", "confirm_password"):
             # user entity doesn't contain these keys
-            request.pop(key)
+            entity.pop(key)
 
-        user = User(id=0, **request, hashed_password=hashed_password)
+        user = User(id=0, hashed_password=hashed_password, **entity)
         self.user_repository.store(user)
+
         return user
