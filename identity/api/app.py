a9f7deaf404029f1188f818c9594b885d6fe2399
Daniel Omiya <danielomiya@gmail.com>
2022-03-29 23:31:03 -0300

    Remove decorator and use Flask built-in functionality to handle errors

--- identity/api/app.py
+++ identity/api/app.py
@@ -1,9 +1,12 @@
 from flask import Flask
 
+from identity.api.handlers.errors import handle_validation_error
 from identity.api.handlers.user import bp as users_bp
 from identity.api.serde.json_serializer import JSONSerializer
+from identity.domain.errors import ValidationError
 
 app = Flask(__name__)
 app.json_encoder = JSONSerializer
 
 app.register_blueprint(users_bp, url_prefix="/api/v1/users")
+app.register_error_handler(ValidationError, handle_validation_error)
