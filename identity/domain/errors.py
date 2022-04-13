722136400704934f7f01dc11398d60d14f32f4a2
Daniel Omiya <danielomiya@gmail.com>
2022-04-12 22:23:21 -0300

    Refactor ValidationError to ValidationException and add AuthException

--- identity/domain/errors.py
+++ identity/domain/errors.py
@@ -9,15 +9,22 @@ class LoquiException(Exception):
     """
 
 
-class ValidationError(LoquiException):
+class AuthException(LoquiException):
+    """
+    Base exception for handling authentication or authorization errors
+    """
+
+
+class ValidationException(LoquiException):
     errors: List[ErrorDescription]
 
     def __init__(self, *args, error: Union[ErrorDescription, List[ErrorDescription]], **kwargs) -> None:
         """
-        ValidationError constructor
+        ValidationException constructor
 
         Args:
             error (Union[ErrorDescription, List[ErrorDescription]]): errors that caused exception
         """
+        self.message = "One or more validation errors occurred"
         self.errors = error if isinstance(error, list) else [error]
         super().__init__(*args, **kwargs)
