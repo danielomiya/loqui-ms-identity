bf50164370f1fa1a3aedff9d586341e741d8cf67
Daniel Omiya <danielomiya@gmail.com>
2022-04-12 22:21:53 -0300

    Add method to UserRepository contract

--- identity/domain/repositories/user_repository.py
+++ identity/domain/repositories/user_repository.py
@@ -1,4 +1,4 @@
-from abc import ABCMeta
+from abc import ABCMeta, abstractmethod
 
 from identity.domain.models.user import User
 from identity.domain.repositories.base_repository import BaseRepository
@@ -8,3 +8,7 @@ class UserRepository(BaseRepository[User], metaclass=ABCMeta):
     """
     Repository of users
     """
+
+    @abstractmethod
+    def get_by_email(self, email: str) -> User:
+        raise NotImplementedError("override me")
